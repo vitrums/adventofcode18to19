ede9a09f9e7e0fd03d23fb60025d68829837140d
Dmitry.Ivanov <Dmitry.Ivanov@jetbrains.com>
2019-02-09 16:46:17 +0300

    problem8 solved

--- src/Problem8.kt
+++ src/Problem8.kt
@@ -4,6 +4,12 @@ import java.nio.file.Paths
 
 class Node(val children: MutableList<Node>, val metadata: MutableList<Int>) {
     fun sumMetadata(): Int = metadata.sum() + children.sumBy { it.sumMetadata() }
+    fun valueOfNode(): Int {
+        return if (children.isEmpty())
+            metadata.sum()
+        else
+            metadata.sumBy { if (it > children.size) 0 else children[it-1].valueOfNode() }
+    }
 
     companion object {
         fun parse(stream: Iterator<Int>) : Node {
@@ -29,6 +35,7 @@ fun main(args: Array<String>) {
         val stream = l.split(" ").map { it.toInt() }
         val root = Node.parse(stream.iterator())
         println(root.sumMetadata())
+        println(root.valueOfNode())
     }
 }
 
