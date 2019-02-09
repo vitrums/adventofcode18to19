020bbaac3eb59899f05c3f26ea49e2f6ceced073
Dmitry.Ivanov <Dmitry.Ivanov@jetbrains.com>
2019-02-09 16:16:27 +0300

    problem8

---
+++ src/Problem8.kt
@@ -0,0 +1,16 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
+class Node(val children: MutableList<Node>, val metadata: List<Int>) {
+    companion object {
+        fun read(Array<int> )
+    }
+}
+
+fun main(args: Array<String>) {
+    val lines = Files.readAllLines(Paths.get("input/7.txt"))
+
+    println(elapsed)
+}
+
