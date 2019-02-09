8380b731be071b370fd129452e38cb37562b3434
Dmitry.Ivanov <Dmitry.Ivanov@jetbrains.com>
2019-02-10 00:32:48 +0300

    problem12 - fixed

--- src/Problem12.kt
+++ src/Problem12.kt
@@ -1,16 +1,19 @@
+import java.math.BigInteger
 import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     val lines = Files.readAllLines(Paths.get("input/12.txt"))
 
-    val n = 50
+    val n = 320
     val placeholder = "".padStart(n, '.')
     var state = placeholder + lines[0] + placeholder
     val rules = MutableList(lines.size - 1) {i ->
         lines[i+1].substring(0..4) to lines[i+1][9]
     }
-    repeat(20) {
+    var prevSum = 0
+    var i = 0
+    repeat(130) {
         val nextState = state.toCharArray().clone()
         cycle@for (i in 0..state.length-5) {
             for ((pattern, subst) in rules)
@@ -22,14 +25,22 @@ fun main(args: Array<String>) {
         }
 
         state = String(nextState)
+
+        val sum = state.foldIndexed(0) {idx, acc, ch ->
+            if (ch == '.') acc else acc+idx-n}
+
+        println("${++i} : ${sum - prevSum}, $sum)")
+        prevSum = sum
     }
 
-    val pots = state.substring(n..(state.length - 1 - n))
+    val pots = state.substring(n..(state.length - 1))
     println(pots)
 
 
     println(state.foldIndexed(0) {idx, acc, ch ->
         if (ch == '.') acc else acc+idx-n}
     )
+
+    println((50_000_000_000L-124L)*88L +11216L )
 }
 
