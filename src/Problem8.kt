93eb02c4ad8739eb86005081ca595381b7937f90
Dmitry.Ivanov <Dmitry.Ivanov@jetbrains.com>
2019-02-09 16:26:06 +0300

    problem8

--- src/Problem8.kt
+++ src/Problem8.kt
@@ -2,15 +2,33 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 
-class Node(val children: MutableList<Node>, val metadata: List<Int>) {
+class Node(val children: MutableList<Node>, val metadata: MutableList<Int>) {
+    fun sumMetadata(): Int = metadata.sum() + children.sumBy { it.sumMetadata() }
+
     companion object {
-        fun read(Array<int> )
+        fun parse(stream: Iterator<Int>) : Node {
+            val nchildren = stream.next()
+            val nmetadata = stream.next()
+
+            val res = Node(mutableListOf(), mutableListOf())
+            repeat(nchildren) {
+                res.children.add(parse(stream))
+            }
+            repeat(nmetadata) {
+                res.metadata.add(stream.next())
+            }
+            return res;
+        }
     }
 }
 
 fun main(args: Array<String>) {
-    val lines = Files.readAllLines(Paths.get("input/7.txt"))
+    val lines = Files.readAllLines(Paths.get("input/8.txt"))
 
-    println(elapsed)
+    for (l in lines) {
+        val stream = l.split(" ").map { it.toInt() }
+        val root = Node.parse(stream.iterator())
+        println(root.sumMetadata())
+    }
 }
 
